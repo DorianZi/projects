542b933003b875810eb3a76316b25b4946f833a3
DorianZi <zyy412@163.com>
2018-12-12 07:54:21 +0800

    Update faceDetecter.py
    whatever face detect

--- face-detect/faceDetecter.py
+++ face-detect/faceDetecter.py
@@ -8,29 +8,19 @@ class faceDetecter():
         pass
     def getFaceFromCamera(self,outdir):
         camera = cv2.VideoCapture(0)
+        success, image = camera.read()
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         haar = cv2.CascadeClassifier('C:\Python27\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
-        success, img = camera.read()
-        print "success = ",success
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = haar.detectMultiScale(gray_img, 1.3, 5)
-        for f_x, f_y, f_w, f_h in faces:
-            face = img[f_y:f_y+f_h, f_x:f_x+f_w]
-            face = cv2.resize(face, (IMGSIZE, IMGSIZE))
-            face = relight(face, random.uniform(0.5, 1.5), random.randint(-50, 50))
-            cv2.imwrite(os.path.join(outdir, str(n)+'.jpg'), face)
-            cv2.putText(img, 'Dorian', (f_x, f_y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, 255, 2)
-            img = cv2.rectangle(img, (f_x, f_y), (f_x + f_w, f_y + f_h), (255, 0, 0), 2)
+        faces = haar.detectMultiScale(gray_img, scaleFactor = 1.15, minNeighbors = 5, minSize = (5,5),)
+        print "face count:",len(faces)
+        for x, y, w, h in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        cv2.imshow('img', img)
-        cv2.waitKey(30)
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
         camera.release()
         #cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     detecter = faceDetecter()
     detecter.getFaceFromCamera("C:\Users\dzi\Pictures")
-    while 1:
-        input = input("input a to stop")
-        if input == 'a':
-            break
-        
