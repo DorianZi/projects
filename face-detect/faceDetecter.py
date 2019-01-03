7e3c852ff142a48995600e00efed87acee1bfc69
DorianZi <zyy412@163.com>
2019-01-03 08:15:50 +0800

    succeed to detect Dorian's face

--- face-detect/faceDetecter.py
+++ face-detect/faceDetecter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import os
+import sys
 import cv2
+#need to install extra module: $pip install opencv-contrib-python
 import numpy as np
 from optparse import OptionParser
 
@@ -14,9 +16,9 @@ class faceDetecter():
         self.owner = owner
         pass
         
-    def catchFacesFromCamera(self,mode="collect"):	
-        if not self.owner in OWNERS:
-            os.exit("Error: owner name unknown!")
+    def catchFacesFromCamera(self,mode="collect"):
+        if mode=="collect" and not self.owner in OWNERS:
+            sys.exit("Error: owner name unknown!")
         camera = cv2.VideoCapture(0)
         haar = cv2.CascadeClassifier('C:\Python27\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
         count = 0
@@ -38,32 +40,36 @@ class faceDetecter():
         #cv2.destroyAllWindows()
 
     def getTrainData(self):
-        self.train_X = np.array([])
-        self.train_Y = np.array([])
-        for root, dir, filename in os.walk(self.pic_repo):
-            if not filename.endswith(".jpg"): 
-                continue
-            index = -1
-            for i in range(len(OWNERS)):
-                if not OWNERS[i] in filename:
+        self.train_X = []
+        self.train_Y = []
+        for root, dirs, files in os.walk(self.pic_repo):
+            for filename in files:
+                if not filename.endswith(".jpg"): 
                     continue
-                index = i
-            if -1 == index:
-                continue
-            file_path = os.join(root, name)
-            face = cv2.imread(file_path)
-            np.append(self.train_X, face)
-            np.append(self.train_Y, index)
+                index = -1
+                for i in range(len(OWNERS)):
+                    if not OWNERS[i] in filename:
+                        continue
+                    index = i
+                    break
+                if -1 == index:
+                    continue
+                file_path = os.path.join(root, filename)
+                face = cv2.imread(file_path,cv2.IMREAD_GRAYSCALE)
+                if not face.shape == (173,173):
+                    face = cv2.resize(face,(173,173))          
+                self.train_X.append(face)
+                self.train_Y.append(index)
 
     def train(self):
         self.model = cv2.face.LBPHFaceRecognizer_create()
         self.model.train(np.asarray(self.train_X),np.asarray(self.train_Y))
 
     def detect(self):
-        camera, image, gray_img, faces = catchFacesFromCamera("detect")
+        camera, image, gray_img, faces = self.catchFacesFromCamera("detect")
         for x, y, w, h in faces:
             pred = self.model.predict(gray_img[y:y + h, x:x + w])
-            pred_name = OWNERS[pred]
+            pred_name = OWNERS[pred[0]]
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(image,pred_name,(x,y-20),cv2.FONT_HERSHEY_SIMPLEX,1,255,2)
         cv2.imshow('image', image)
@@ -82,12 +88,13 @@ if __name__ == '__main__':
                   help="detect face") 
     (options, args) = parser.parse_args() 
 
-	assert options.collect and options.detect == False
+    assert (options.collect and options.detect) == False
     if options.collect: 
         detecter = faceDetecter("dorian", "C:\Users\dzi\Pictures")
         detecter.collectFacesFromCamera()
     if options.detect:    
-	    detecter = faceDetecter("", "C:\Users\dzi\Pictures")
+        detecter = faceDetecter("", "C:\Users\dzi\Pictures")
+        detecter.getTrainData()
         detecter.train()
         detecter.detect()
 
