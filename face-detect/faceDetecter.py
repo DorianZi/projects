30eb32b8521bac79ba9a0ddc070577afb1d5c79c
DorianZi <zyy412@163.com>
2019-01-16 07:58:47 +0800

    Get face box through caffe model

--- face-detect/faceDetecter.py
+++ face-detect/faceDetecter.py
@@ -12,6 +12,8 @@ from optparse import OptionParser
 
 DATA_DIR = "C:\Users\dzi\Pictures"
 HAAR_DIR = "C:\Python27\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml"
+PROTOTXT_FILE = "deploy.prototxt"
+MODEL_FILE = "res10_300x300_ssd_iter_140000.caffemodel"
 
 def ERR_EXIT(err=""):
     if err: 
@@ -22,22 +24,34 @@ class faceDetecter():
     def __init__(self,owner,picDir):
         self.pic_repo = picDir
         self.owner = owner
-        self.haar = cv2.CascadeClassifier(HAAR_DIR)
-        
+        modelDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "model")
+        prototxtPath = os.path.join(modelDir, PROTOTXT_FILE)
+        modePath = os.path.join(modelDir, MODEL_FILE)
+        self.caffe_detector = cv2.dnn.readNetFromCaffe(prototxtPath, modePath)
+
     def catchFacesFromCamera(self,camera,mode="collect"):
         count = 0
         for n in range(30):
             success, image = camera.read()
             gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            faces = self.haar.detectMultiScale(gray_img, scaleFactor = 1.15, minNeighbors = 5, minSize = (5,5))
-            if not len(faces):
-                print "No face detected!"
+            (h, w) = image.shape[:2]
+            imageBlob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), 
+                                               (104.0, 177.0, 123.0), swapRB=False, crop=False)
+            self.caffe_detector.setInput(imageBlob)
+            detections = self.caffe_detector.forward()
+            faceBoxList = []
+            for i in range(detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
+                if confidence >  0.5:
+                    faceBox = (detections[0, 0, i, 3:7] * np.array([w, h, w, h])).astype("int")
+                    (startX, startY, endX, endY) = faceBox
+                    faceBoxList.append(faceBox)
             if mode == "detect":
-                return image, gray_img, faces
-            for x, y, w, h in faces:
+                return image, gray_img, faceBoxList
+            for startX, startY, endX, endY in faceBoxList:
                 count = count + 1
-                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                cv2.imwrite(os.path.join(self.pic_repo,"train_image_{0}_{1}.jpg".format(self.owner,count)), cv2.resize(gray_img[y:y + h, x:x + w],(200,200)))
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                cv2.imwrite(os.path.join(self.pic_repo,"train_image_{0}_{1}.jpg".format(self.owner,count)), cv2.resize(gray_img[startY:endY, startX:endX],(300,300)))
             cv2.imshow('image', image)
             cv2.waitKey(0)
         #cv2.destroyAllWindows()
@@ -48,10 +62,18 @@ class faceDetecter():
             pic_path = os.path.join(self.pic_repo,pic)
         image = cv2.imread(pic_path)
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = self.haar.detectMultiScale(gray_img, scaleFactor = 1.15, minNeighbors = 5, minSize = (5,5))
-        if not len(faces):
-            print "No face detected!"
-        return image,gray_img,faces
+        (h, w) = image.shape[:2]
+        imageBlob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), 
+                                           (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        self.caffe_detector.setInput(imageBlob)
+        detections = self.caffe_detector.forward()
+        faceBoxList = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence >  0.5:
+                faceBox = (detections[0, 0, i, 3:7] * np.array([w, h, w, h])).astype("int")
+                faceBoxList.append(faceBox)
+        return image,gray_img,faceBoxList
 
     def getTrainData(self):
         self.train_X = []
@@ -69,7 +91,7 @@ class faceDetecter():
                     self.labelDict[owner] = label_n 
                 file_path = os.path.join(root, filename)
                 face_img = cv2.imread(file_path,cv2.IMREAD_GRAYSCALE)
-                face_img = cv2.resize(face_img,(200,200))
+                face_img = cv2.resize(face_img,(300,300))
                 self.train_X.append(face_img)
                 self.train_Y.append(label_n)
         self.train_X, self.train_Y = np.asarray(self.train_X), np.asarray(self.train_Y)
@@ -81,21 +103,22 @@ class faceDetecter():
         self.model = SVC(C=1.0, kernel="linear", probability=True)
         self.model.fit(self.train_X.reshape(self.train_X.shape[0],-1), self.train_Y)
 
+
     def detect(self,source="",camera="",pic="",video=False):
         if source == "camera":
             image, gray_img, faces = self.catchFacesFromCamera(camera,"detect")
         elif source == "picture":
             image, gray_img, faces = self.catchFacesFromPicture(pic)
         pred_dict = dict([(v,k) for k,v in self.labelDict.items()])
-        for x, y, w, h in faces:
-            target = cv2.resize(gray_img[y:y + h, x:x + w],(200,200)).reshape(1,-1)
+        for startX, startY, endX, endY in faces:
+            target = cv2.resize(gray_img[startY:endY, startX:endX],(300,300)).reshape(1,-1)
             pred = self.model.predict(target)
             pred_proba = self.model.predict_proba(target)[0][pred[0]-1]
             pred_name = pred_dict[pred[0]]
             show_text = pred_name + " %.2f%%" % (100*float(pred_proba))
             print pred,pred_proba
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(image,show_text,(x,y-20),cv2.FONT_HERSHEY_SIMPLEX,1,255,2)
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            cv2.putText(image,show_text,(startX,startY-20),cv2.FONT_HERSHEY_SIMPLEX,1,255,2)
         cv2.imshow('image', image)
         if video:
             if not cv2.waitKey(10) == -1:
@@ -141,13 +164,13 @@ if __name__ == '__main__':
         detecter.getTrainData()
         detecter.train()
         if options.pic:
-            detecter.detect("picture",pic=options.pic)
+            detecter.detect(source="picture",pic=options.pic)
         else:
             cmr = cv2.VideoCapture(0)
             if options.video:
                 while 1:
-                    if not detecter.detect("camera",camera=cmr,video=True):
+                    if not detecter.detect(source="camera",camera=cmr,video=True):
                         break
             else:
-                detecter.detect("camera",camera=cmr)
+                detecter.detect(source="camera",camera=cmr)
             cmr.release()
